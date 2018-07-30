d0f81ea6def8307a155515d694752a2e08a078c5
Rajorshi Chaudhuri <rajorshi01@gmail.com>
2018-07-30 22:16:59 +0530

    Update rnn.py

--- models/rnn.py
+++ models/rnn.py
@@ -5,14 +5,14 @@ from torch import optim
 
 class RNN(nn.Module):
   '''
-    Uses two layered bidirectional GRU
+  Uses 3 layered bidirectional GRU
   '''
   def __init__(self, input_size, hidden_size, output_size):
     super(RNN, self).__init__()
     self.hidden_size = hidden_size
     
     self.embedding = nn.Embedding.from_pretrained(weights)
-    self.gru = nn.GRU(hidden_size, hidden_size, num_layers=2, bidirectional=True)
+    self.gru = nn.GRU(hidden_size, hidden_size, num_layers=3, bidirectional=True)
     self.out = nn.Linear(hidden_size*2, output_size)
     self.softmax = nn.LogSoftmax(dim=2)
   
@@ -24,4 +24,4 @@ class RNN(nn.Module):
     return output, hidden
   
   def initHidden(self):
-    return torch.zeros(4, 1, self.hidden_size)
+    return torch.zeros(6, 1, self.hidden_size, device=device)
